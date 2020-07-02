a3ed79cfe25d21eee797bb10f9d7e76dc3f59b08
odeane_croney <odeane_croney@yahoo.com>
2020-07-03 01:13:26 +0200

    rearraging image location

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header.component.jsx'
-import ImageList from './components/image_list/image_list.component';
+import './components/image_list/image_list.style.css'
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userScore: 0
+    }
+  }
+
+
+
+
+
   render() {
     return (
       <div className="App">
         < Header />
-        <ImageList />
-      
+       
+       
+        <div className="hands">
+
+          <div id="rock-btn" onClick={() => this.setState({ string: 'hey' })} >
+            <img src="/images/rock.png" alt="" />
+          </div>
+
+          <div id='paper-btn'>
+            <img src="/images/paper.png" />
+          </div>
+
+          <div id='scissors-btn'>
+            <img src='/images/scissors.png'></img>
+          </div>
+
+
+        </div>
+        
+
+        <p>{this.state.score}</p>
       </div>
     )
   }
