ce79d287c617b317bdadc6b0622e8febe3e130ba
odeane_croney <odeane_croney@yahoo.com>
2020-07-06 22:57:45 +0200

    adding game funtionality and adding weapons selective button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header.component.jsx'
-import './components/image_list/image_list.style.css'
+// import './components/image_list/image_list.style.css'
 
+
+const weapons = ['rock', 'paper', 'scissors']
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      userScore: 0
+      userOne: '',
+      computer: ''
     }
   }
 
+  playGame = () => {
+    this.setState({
+      computer: weapons[Math.floor(Math.random() * 3)]
+    });
+  }
 
+  
+
+  
 
 
 
@@ -20,28 +31,27 @@ class App extends Component {
     return (
       <div className="App">
         < Header />
-       
-       
+
         <div className="hands">
 
-          <div id="rock-btn" onClick={() => this.setState({ string: 'hey' })} >
-            <img src="/images/rock.png" alt="" />
+          <div id="rock-btn" >
+            <img src="/images/rock.png" alt="rock" onClick ={() => this.setState({userOne: weapons[0]}) }/>
           </div>
 
           <div id='paper-btn'>
-            <img src="/images/paper.png" />
+            <img src="/images/paper.png" alt="paper" onClick={() => this.setState({ userOne: weapons[1] })}/>
           </div>
 
           <div id='scissors-btn'>
-            <img src='/images/scissors.png'></img>
+            <img src='/images/scissors.png' alt="scissors" onClick={() => this.setState({ userOne: weapons[3] })}/>
           </div>
 
 
         </div>
-        
-
-        <p>{this.state.score}</p>
+        <h1>{this.state.computer}</h1>
+        <button type='button' onClick={this.playGame} >Play</button>
       </div>
+
     )
   }
 }
